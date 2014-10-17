46c8c94143a02d4e7194eedc5a58a840e4f9548a
Purnendu_iPhone <apple@Rajkumar-iPhone.local>
2014-10-17 18:45:38 +0530

    new

--- GitUseExample/ViewController.h
+++ GitUseExample/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)Click:(id)sender;
+- (IBAction)btnGit:(id)sender;
 
 @end
 
--- GitUseExample/ViewController.m
+++ GitUseExample/ViewController.m
@@ -24,4 +24,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)Click:(id)sender {
+    NSLog(@"This is a Git tutorial");
+    
+    
+    
+}
+
+- (IBAction)btnGit:(id)sender {
+    
+    [[[UIAlertView alloc]initWithTitle:@"Hello" message:@"TO GIT EXAMPLE" delegate:self cancelButtonTitle:@"GO" otherButtonTitles:nil, nil]show];
+    
+}
 @end
